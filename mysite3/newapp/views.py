1a5dc028b106b64bc539a7e53acffd805bd9bf80
Permadi Ajus <permadiajus@gmail.com>
2022-09-22 16:50:47 +0800

    Membuat Pagination

--- mysite3/newapp/views.py
+++ mysite3/newapp/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from .models import Movies
+from django.core.paginator import Paginator
 # Create your views here.
 
 
 def movie_list(request):
     movies_object = Movies.objects.all()
+    paginator = Paginator(movies_object, 2)
+    page = request.GET.get('page')
+    movies_object = paginator.get_page(page)
     context = {
         'movies_object': movies_object
     }
