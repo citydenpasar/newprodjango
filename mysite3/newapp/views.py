e23004f57f81d5e73c8643bf4e1dea400c46026b
Permadi Ajus <permadiajus@gmail.com>
2022-09-26 16:07:22 +0800

    Search dan Filter

--- mysite3/newapp/views.py
+++ mysite3/newapp/views.py
@@ -6,6 +6,11 @@ from django.core.paginator import Paginator
 
 def movie_list(request):
     movies_object = Movies.objects.all()
+    #pelajaran search
+    movie_name = request.GET.get('movie_name')
+    if movie_name !='' and movie_name is not None:
+        # movies_object = Movies.objects.filter(name=movie_name) #filter biasa
+        movies_object = Movies.objects.filter(name__icontains=movie_name)
     paginator = Paginator(movies_object, 2)
     page = request.GET.get('page')
     movies_object = paginator.get_page(page)
